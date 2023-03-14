c07a748bae4326933bc5772b3c9672a2977a295d
Ronaldo Arrais <arraisronaldo6@gmail.com>
2023-03-14 06:55:35 -0300

    Atividade concluída

---
+++ main.py
@@ -0,0 +1,6 @@
+var1 = int(input("Qual o ano atual? "))
+var2 = int(input("Qual a sua idade atual? "))
+var3 = int(input("Digite um ano aleatório: "))
+var4 = input("Digite seu nome: ")
+
+print(f"{var4}, no ano de {var3} você teria {var2+(var3-var1)} anos")
